4b6c55d348a69b3eafd496b1efe40d6ecee22b39
CristinaMarin490 <78991431+CristinaMarin490@users.noreply.github.com>
2021-08-29 15:49:53 -0500

    Add Table Brand

--- Vehicles.API/Migrations/20210829194433_AddTableBrand.Designer.cs
+++ Vehicles.API/Migrations/20210829194433_AddTableBrand.Designer.cs
@@ -9,8 +9,8 @@ using Vehicles.API.Data;
 namespace Vehicles.API.Migrations
 {
     [DbContext(typeof(DataContext))]
-    [Migration("20210829155807_AddTableProcedures_")]
-    partial class AddTableProcedures_
+    [Migration("20210829194433_AddTableBrand")]
+    partial class AddTableBrand
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
@@ -19,6 +19,26 @@ namespace Vehicles.API.Migrations
                 .HasAnnotation("Relational:MaxIdentifierLength", 128)
                 .HasAnnotation("ProductVersion", "5.0.9")
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("Vehicles.API.Data.Entities.Brand", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("int")
+                        .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+                    b.Property<string>("Description")
+                        .IsRequired()
+                        .HasMaxLength(50)
+                        .HasColumnType("nvarchar(50)");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("Description")
+                        .IsUnique();
+
+                    b.ToTable("Brands");
+                });
 
             modelBuilder.Entity("Vehicles.API.Data.Entities.Procedure", b =>
                 {
--- Vehicles.API/Migrations/20210829194433_AddTableBrand.cs
+++ Vehicles.API/Migrations/20210829194433_AddTableBrand.cs
@@ -2,27 +2,26 @@
 
 namespace Vehicles.API.Migrations
 {
-    public partial class AddTableProcedures_ : Migration
+    public partial class AddTableBrand : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.CreateTable(
-                name: "Procedures",
+                name: "Brands",
                 columns: table => new
                 {
                     Id = table.Column<int>(type: "int", nullable: false)
                         .Annotation("SqlServer:Identity", "1, 1"),
-                    Description = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
-                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                    Description = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
                 },
                 constraints: table =>
                 {
-                    table.PrimaryKey("PK_Procedures", x => x.Id);
+                    table.PrimaryKey("PK_Brands", x => x.Id);
                 });
 
             migrationBuilder.CreateIndex(
-                name: "IX_Procedures_Description",
-                table: "Procedures",
+                name: "IX_Brands_Description",
+                table: "Brands",
                 column: "Description",
                 unique: true);
         }
@@ -30,7 +29,7 @@ namespace Vehicles.API.Migrations
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropTable(
-                name: "Procedures");
+                name: "Brands");
         }
     }
 }
